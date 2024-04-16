8a2ea57c7457fc908c65054d1fee93a618ab76b1
Roman Koshel <roma.koshel@gmail.com>
2024-04-16 10:27:54 +0300

    Fix examples

--- examples/tasks.py
+++ examples/tasks.py
@@ -15,12 +15,6 @@ async def foo():
     arrlio.logger.info("Hello from task 'foo'!")
 
 
-@arrlio.task(bind=True)
-async def bind(self):
-    arrlio.logger.info(self.data.task_id)
-    arrlio.logger.info(self)
-
-
 @arrlio.task
 async def exception():
     raise ZeroDivisionError
